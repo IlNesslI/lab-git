567052a2a814ebc07070d4439e62dcb70b905cf3
IlNesslI <polina.khlyzova08@gmail.com>
2026-02-08 16:42:37 +0600

    Update Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,8 +1,2 @@
 ﻿
-<<<<<<< HEAD
 Console.WriteLine("Hello, мир!");
-=======
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
->>>>>>> eb391843e0e83974309f09a2860e666b35829753
